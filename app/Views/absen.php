faf2997c4247f0bb3ba2f6d32e51c33478b22fc9
RiqqiAmru <Riqqia00@gmail.com>
2023-05-01 23:49:59 +0700

    menambahkan status jam di presensi (hrd)

--- app/Views/absen.php
+++ app/Views/absen.php
@@ -2,8 +2,7 @@
 <?= $this->section('content') ?>
 <div class="row">
   <p>
-    <a class="btn btn-info" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false"
-      aria-controls="collapseExample">
+    <a class="btn btn-info" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
       Tata Tertib Absen
     </a>
   </p>
@@ -36,13 +35,15 @@
     <div class="card-body">
       <h5 class="card-title">Absen Harian</h5>
       <hr>
-      <p><span class="tanggal"></span> | <span class="jam"></span></p>
+      <p><span class="tanggal"></span></p>
       <form action="" method="post">
         <!-- input tipe capture untuk foto masuk -->
         <video autoplay="true" id="video-webcam" class="object-fit-md-contain border rounded">
           browser tidak mendukung untuk pengambilan gambar
         </video><br>
-        <button onclick="takeSnapshot()" class="btn btn-success">Ambil Gambar</button>
+
+        <button onclick="" class="btn btn-success">Hadir</button>
+        <button class="btn btn-warning">Ijin</button>
       </form>
     </div>
   </div>
@@ -52,62 +53,68 @@
 
 
 <script type="text/javascript">
-// seleksi elemen video
-var video = document.querySelector("#video-webcam");
+  // seleksi elemen video
+  var video = document.querySelector("#video-webcam");
 
-// // minta izin user
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
-  navigator.msGetUserMedia || navigator.oGetUserMedia;
+  // // minta izin user
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia || navigator.oGetUserMedia;
 
-function takeSnapshot() {
-  // buat elemen img
-  var img = document.createElement('img');
-  var context;
+  function takeSnapshot() {
+    // buat elemen img
+    var img = document.createElement('img');
+    var context;
 
-  // ambil ukuran video
-  var width = video.offsetWidth,
-    height = video.offsetHeight;
+    // ambil ukuran video
+    var width = video.offsetWidth,
+      height = video.offsetHeight;
 
-  // buat elemen canvas
-  canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
+    // buat elemen canvas
+    canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
 
-  // ambil gambar dari video dan masukan 
-  // ke dalam canvas
-  context = canvas.getContext('2d');
-  context.drawImage(video, 0, 0, width, height);
+    // ambil gambar dari video dan masukan 
+    // ke dalam canvas
+    context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, width, height);
 
-  // render hasil dari canvas ke elemen img
-  img.src = canvas.toDataURL('image/png');
-  document.body.appendChild(img);
-}
+    // render hasil dari canvas ke elemen img
+    img.src = canvas.toDataURL('image/png');
+    // isikan elemen img ke dalam form
+    document.querySelector('#mydata').value = img.src;
+    // document.body.appendChild(img);
+  }
 
-// ini dari chatgpt
-navigator.mediaDevices.getUserMedia({
-    video: true
-  })
-  .then(stream => {
-    video.srcObject = stream;
-    const track = stream.getVideoTracks()[0];
-    const settings = track.getSettings();
-    const aspectRatio = settings.aspectRatio; // nilai aspek rasio
-    console.log(settings);
+  // ini dari chatgpt
+  navigator.mediaDevices.getUserMedia({
+      video: true
+    })
+    .then(stream => {
+      video.srcObject = stream;
+      const track = stream.getVideoTracks()[0];
+      const settings = track.getSettings();
+      const aspectRatio = settings.aspectRatio; // nilai aspek rasio
+      console.log(settings);
 
 
-    video.height = 240;
+      video.height = 240;
 
-    // atur ukuran elemen video sesuai dengan aspek rasio
-    if (aspectRatio > 1) {
-      video.width = video.height * aspectRatio;
-    } else {
-      video.height = video.width / aspectRatio;
-    }
-  })
-  .catch(error => console.log(error));
+      // atur ukuran elemen video sesuai dengan aspek rasio
+      if (aspectRatio > 1) {
+        video.width = video.height * aspectRatio;
+      } else {
+        video.height = video.width / aspectRatio;
+      }
+    })
+    .catch(error => console.log(error));
 
-//ambil tanggal & jam hari ini yang di refresh otomatis per detik
-document.querySelector(".tanggal").innerHTML = moment().locale('id').format('dddd, DD MMMM YYYY');
+  //ambil tanggal & jam hari ini yang di refresh otomatis per detik
+  function jamAbsen() {
+    document.querySelector(".tanggal").innerHTML = moment().locale('id').format('dddd, DD MMMM YYYY | hh:mm:ss');
+  }
+
+  setInterval(jamAbsen, 1000);
 </script>
 
 <?= $this->endSection(); ?>
