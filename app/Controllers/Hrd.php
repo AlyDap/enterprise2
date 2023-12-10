81c169467a2ef395802a9268873e7b2646974f46
AlyDap <ferdynan.jr@gmail.com>
2023-12-10 21:54:05 +0700

    ubah posisi dan jarak

--- app/Controllers/Hrd.php
+++ app/Controllers/Hrd.php
@@ -7,101 +7,105 @@ use CodeIgniter\Controller;
 
 class Hrd extends BaseController
 {
-    protected $userModel;
-    protected $penggajian;
-    protected $presensiModel;
-    public function __construct()
-    {
-        $this->userModel = new \App\Models\User();
-        $this->penggajian = new \App\Controllers\Penggajian();
-        $this->presensiModel = new \App\Models\presensiModel();
+ protected $userModel;
+ protected $penggajian;
+ protected $presensiModel;
+ public function __construct()
+ {
+  $this->userModel = new \App\Models\User();
+  $this->penggajian = new \App\Controllers\Penggajian();
+  $this->presensiModel = new \App\Models\presensiModel();
+ }
+
+ public function index()
+ {
+  if (session('jabatan') != 'hrd')
+   return redirect()->to('/dashboard');
+
+  $totalUser = $this->userModel->countAll();
+
+  $penggajian = $this->penggajian->hitungGaji();
+
+  $absensi = $this->presensiModel->where('tanggal_presensi', date('Y-m-d'))->findAll();
+
+  foreach ($penggajian as $p) {
+   foreach ($absensi as $a) {
+    if ($p['id_pegawai'] == $a['id_pegawai']) {
+     $penggajian[$p['id_pegawai']]['info'] = $a['info'];
+    } else {
+     $penggajian[$p['id_pegawai']]['info'] = 'belum absen';
     }
+   }
+  }
+  // dd($absensi);
 
-    public function index()
-    {
-        if (session('jabatan') != 'hrd') return redirect()->to('/dashboard');
+  $data = [
+   'title' => 'Dashboard',
+   'totalUser' => $totalUser,
+   'penggajian' => $penggajian,
+  ];
+  // hrd/index itu laman dashboard
+  return view('hrd/index', $data);
+ }
 
-        $totalUser = $this->userModel->countAll();
-
-        $penggajian = $this->penggajian->hitungGaji();
-
-        $absensi = $this->presensiModel->where('tanggal_presensi', date('Y-m-d'))->findAll();
-
-        foreach ($penggajian as $p) {
-            foreach ($absensi as $a) {
-                if ($p['id_pegawai'] == $a['id_pegawai']) {
-                    $penggajian[$p['id_pegawai']]['info']   = $a['info'];
-                } else {
-                    $penggajian[$p['id_pegawai']]['info']   = 'belum absen';
-                }
-            }
-        }
-        // dd($absensi);
-
-        $data = [
-            'title' => 'Dashboard',
-            'totalUser' => $totalUser,
-            'penggajian' => $penggajian,
-        ];
-        // hrd/index itu laman dashboard
-        return view('hrd/index', $data);
-    }
-
-    public function tampil()
-    {
-        if (session('jabatan') != 'hrd') return redirect()->to('/dashboard');
-        $data = [
-            'title' => 'HRD',
-            // Menampilkan daftar user
-            'users' => $this->userModel->findAll()
-        ];
-        return view('hrd/tampil', $data);
-    }
+ public function tampil()
+ {
+  if (session('jabatan') != 'hrd')
+   return redirect()->to('/dashboard');
+  $data = [
+   'title' => 'HRD',
+   // Menampilkan daftar user
+   'users' => $this->userModel->findAll()
+  ];
+  return view('hrd/tampil', $data);
+ }
 
 
-    public function store()
-    {
-        if (session('jabatan') != 'hrd') return redirect()->to('/dashboard');
-        $id = $this->request->getVar('id-user');
-        $session = session();
-        if ($id == '') { //ini berarti tambah data
+ public function store()
+ {
+  if (session('jabatan') != 'hrd')
+   return redirect()->to('/dashboard');
+  $id = $this->request->getVar('id-user');
+  $session = session();
+  if ($id == '') { //ini berarti tambah data
 
-            // Mengambil data dari form input
-            $data = [
-                'username' => $this->request->getVar('username'),
-                'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
-                'jabatan' => $this->request->getVar('jabatan'),
-                'gaji' => $this->request->getVar('gaji')
-            ];
+   // Mengambil data dari form input
+   $data = [
+    'username' => $this->request->getVar('username'),
+    'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
+    'jabatan' => $this->request->getVar('jabatan'),
+    'gaji' => $this->request->getVar('gaji')
+   ];
 
-            // Memasukkan data ke dalam database
-            $this->userModel->insert($data);
+   // Memasukkan data ke dalam database
+   $this->userModel->insert($data);
 
-            $session->set('alert', 'success');
-        } else { //ini berarti edit data
-            $this->update($id);
-            $session->set('alert', 'edit');
-        }
-        // Mengarahkan pengguna kembali ke halaman daftar user
-        return redirect()->to('/hrd/tampil');
-    }
+   $session->set('alert', 'success');
+  } else { //ini berarti edit data
+   $this->update($id);
+   $session->set('alert', 'edit');
+  }
+  // Mengarahkan pengguna kembali ke halaman daftar user
+  return redirect()->to('/hrd/tampil');
+ }
 
 
-    public function update($id)
-    {
-        if (session('jabatan') != 'hrd') return redirect()->to('/dashboard');
-        // Mengambil data dari form input
-        $data = [
-            'username' => $this->request->getVar('username'),
-            'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
-            'jabatan' => $this->request->getVar('jabatan'),
-            'gaji' => $this->request->getVar('gaji')
-        ];
-        if ($this->request->getVar('password') == '') {
-            unset($data['password']);
-        }
+ public function update($id)
+ {
+  if (session('jabatan') != 'hrd')
+   return redirect()->to('/dashboard');
+  // Mengambil data dari form input
+  $data = [
+   'username' => $this->request->getVar('username'),
+   'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
+   'jabatan' => $this->request->getVar('jabatan'),
+   'gaji' => $this->request->getVar('gaji')
+  ];
+  if ($this->request->getVar('password') == '') {
+   unset($data['password']);
+  }
 
-        // Memperbarui data ke dalam database
-        $this->userModel->update($id, $data);
-    }
+  // Memperbarui data ke dalam database
+  $this->userModel->update($id, $data);
+ }
 }
--- app/Views/hrd/tampil.php
+++ app/Views/hrd/tampil.php
@@ -2,113 +2,119 @@
 <?= $this->section('content') ?>
 <?= $disabled = '' ?>
 <div class="row">
-  <?php if (session()->get('alert') == 'success') : ?>
+ <?php if (session()->get('alert') == 'success') : ?>
   <div class="alert alert-success alert-dismissible fade show" role="alert">
-    <i class="fa-regular fa-badge-check fa-bounce"></i>
-    Data user berhasil ditambahkan!
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+   <i class="fa-regular fa-badge-check fa-bounce"></i>
+   Data user berhasil ditambahkan!
+   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div>
-  <?php elseif (session()->get('alert') == 'edit') : ?>
+ <?php elseif (session()->get('alert') == 'edit') : ?>
   <div class="alert alert-primary alert-dismissible fade show" role="alert">
-    <i class="fa-regular fa-badge-check fa-bounce"></i>
-    Data user berhasil di Update!
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+   <i class="fa-regular fa-badge-check fa-bounce"></i>
+   Data user berhasil di Update!
+   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div>
-  <?php session()->remove('alert')  ?>
-  <?php endif ?>
+  <?php session()->remove('alert') ?>
+ <?php endif ?>
 
 
-  <div class="col-sm-8">
-    <h1 class="mt-2">Daftar User</h1>
+ <div class="col-sm-8">
+  <h1 class="mt-2">Daftar User</h1>
 
-    <table class="table table-bordered table-hover">
-      <thead>
-        <tr>
-          <th scope="col">NO</th>
-          <th scope="col">Username</th>
-          <th scope="col">Jabatan</th>
-          <th scope="col">Gaji</th>
-          <th scope="col">Aksi</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php $no = 1; ?>
-        <?php foreach ($users as $user) : ?>
-        <tr>
-          <th scope="row"><?= $no++; ?></th>
-          <td><?= $user['username']; ?></td>
-          <td><?= $user['jabatan']; ?></td>
-          <td><?= "Rp " . number_format($user['gaji'], 0, ',', '.');  ?></td>
-          <?php ($user['username'] == 'ali') ? $disabled = 'disabled' : $disabled = '' ?>
-          <td>
-            <a class="btn btn-primary btn-sm <?= $disabled; ?>"
-              onclick="editData(<?= $user['id_user']; ?>,`<?= $user['username']; ?>`,`<?= $user['jabatan']; ?>`,`<?= $user['gaji']; ?>`)"
-              id="btn-edit">Edit</a>
-          </td>
-        </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
+  <table class="table table-bordered table-hover">
+   <thead>
+    <tr>
+     <th scope="col">NO</th>
+     <th scope="col">Username</th>
+     <th scope="col">Jabatan</th>
+     <th scope="col">Gaji</th>
+     <th scope="col">Aksi</th>
+    </tr>
+   </thead>
+   <tbody>
+    <?php $no = 1; ?>
+    <?php foreach ($users as $user) : ?>
+     <tr>
+      <th scope="row">
+       <?= $no++; ?>
+      </th>
+      <td>
+       <?= $user['username']; ?>
+      </td>
+      <td>
+       <?= $user['jabatan']; ?>
+      </td>
+      <td>
+       <?= "Rp " . number_format($user['gaji'], 0, ',', '.'); ?>
+      </td>
+      <?php ($user['username'] == 'ali') ? $disabled = 'disabled' : $disabled = '' ?>
+      <td>
+       <a class="btn btn-primary btn-sm <?= $disabled; ?>" onclick="editData(<?= $user['id_user']; ?>,`<?= $user['username']; ?>`,`<?= $user['jabatan']; ?>`,`<?= $user['gaji']; ?>`)" id="btn-edit">Edit</a>
+      </td>
+     </tr>
+    <?php endforeach; ?>
+   </tbody>
+  </table>
+ </div>
+ <div class="col-md-4">
+  <button class="btn btn-primary my-2 position-relative" id="btn-tambah">Tambah User</button>
+  <div class="card mt-3" id="card-form" style="display: none;">
+   <div class="card-body">
+    <h5 class="card-title" id="card-title">Tambah Data</h5>
+
+    <form action="<?= base_url('/hrd/store'); ?>" method="post">
+     <input type="hidden" id="id-user" name="id-user" value="">
+     <div class="mb-3">
+      <label for="username" class="form-label">Username</label>
+      <input type="text" class="form-control" id="username" name="username" required value="">
+     </div>
+     <div class="mb-3">
+      <label for="password" class="form-label" id="lbl-pass">Password</label>
+      <input type="password" class="form-control" name="password" required value="" id="password">
+     </div>
+     <div class="mb-3">
+      <label for="jabatan" class="form-label">Jabatan</label>
+      <select class="form-select" id="jabatan" name="jabatan" required>
+       <option value="">-- Pilih Jabatan --</option>
+       <option value="produksi">Bag. Produksi</option>
+       <option value="gudang">Bag. Gudang</option>
+       <option value="penjualan">Bag. Penjualan</option>
+       <option value="hrd">Bag. Hrd</option>
+       <option value="finance">Bag. Finance</option>
+      </select>
+     </div>
+     <div class="mb-3">
+      <label for="gaji" class="form-label">Gaji</label>
+      <input type="number" class="form-control" id="gaji" name="gaji" required>
+     </div>
+     <button type="submit" class="btn btn-primary" id="btn-form">Simpan</button>
+     <button type="button" class="btn btn-secondary" id="btn-batal">Batal</button>
+    </form>
+   </div>
   </div>
-  <div class="col-md-4">
-    <button class="btn btn-primary my-2 position-relative" id="btn-tambah">Tambah User</button>
-    <div class="card mt-3" id="card-form" style="display: none;">
-      <div class="card-body">
-        <h5 class="card-title" id="card-title">Tambah Data</h5>
-
-        <form action="<?= base_url('/hrd/store'); ?>" method="post">
-          <input type="hidden" id="id-user" name="id-user" value="">
-          <div class="mb-3">
-            <label for="username" class="form-label">Username</label>
-            <input type="text" class="form-control" id="username" name="username" required value="">
-          </div>
-          <div class="mb-3">
-            <label for="password" class="form-label" id="lbl-pass">Password</label>
-            <input type="password" class="form-control" name="password" required value="" id="password">
-          </div>
-          <div class="mb-3">
-            <label for="jabatan" class="form-label">Jabatan</label>
-            <select class="form-select" id="jabatan" name="jabatan" required>
-              <option value="">-- Pilih Jabatan --</option>
-              <option value="produksi">Bag. Produksi</option>
-              <option value="gudang">Bag. Gudang</option>
-              <option value="penjualan">Bag. Penjualan</option>
-              <option value="hrd">Bag. Hrd</option>
-              <option value="finance">Bag. Finance</option>
-            </select>
-          </div>
-          <div class="mb-3">
-            <label for="gaji" class="form-label">Gaji</label>
-            <input type="number" class="form-control" id="gaji" name="gaji" required>
-          </div>
-          <button type="submit" class="btn btn-primary" id="btn-form">Simpan</button>
-          <button type="button" class="btn btn-secondary" id="btn-batal">Batal</button>
-        </form>
-      </div>
-    </div>
-  </div>
+ </div>
 </div>
 
 
 <script>
-const btnTambah = document.querySelector('#btn-tambah');
-const cardForm = document.querySelector('#card-form');
-const btnBatal = document.querySelector('#btn-batal');
-const btnEdit = document.querySelector('#btn-edit');
-const cardTitle = document.querySelector('#card-title');
-const elId = document.querySelector('#id-user');
-const elUsername = document.querySelector('#username');
-const elPass = document.querySelector('#password');
-const lblPass = document.querySelector('#lbl-pass');
-const elJabatan = document.querySelector('#jabatan');
-const elGaji = document.querySelector('#gaji');
-const btnForm = document.querySelector('#btn-form');
+ const btnTambah = document.querySelector('#btn-tambah');
+ const cardForm = document.querySelector('#card-form');
+ const btnBatal = document.querySelector('#btn-batal');
+ const btnEdit = document.querySelector('#btn-edit');
+ const cardTitle = document.querySelector('#card-title');
+ const elId = document.querySelector('#id-user');
+ const elUsername = document.querySelector('#username');
+ const elPass = document.querySelector('#password');
+ const lblPass = document.querySelector('#lbl-pass');
+ const elJabatan = document.querySelector('#jabatan');
+ const elGaji = document.querySelector('#gaji');
+ const btnForm = document.querySelector('#btn-form');
 
-btnTambah.addEventListener('click', function() {
+ btnTambah.addEventListener('click', function() {
   cardForm.style.display = 'block';
-});
+ });
 
-btnBatal.addEventListener('click', function() {
+ btnBatal.addEventListener('click', function() {
   btnTambah.style.display = 'block';
 
   cardTitle.innerHTML = 'Tambah Data';
@@ -122,9 +128,9 @@ btnBatal.addEventListener('click', function() {
   btnForm.innerHTML = 'Simpan';
 
   cardForm.style.display = 'none';
-});
+ });
 
-function editData(id, username, jabatan, gaji) {
+ function editData(id, username, jabatan, gaji) {
   cardForm.style.display = 'block';
   btnTambah.style.display = 'none';
 
@@ -141,10 +147,10 @@ function editData(id, username, jabatan, gaji) {
   btnForm.innerHTML = 'Update';
 
   if (jabatan == 'hrd') {
-    elGaji.setAttribute('readonly', true);
+   elGaji.setAttribute('readonly', true);
   } else {
-    elGaji.removeAttribute('readonly');
+   elGaji.removeAttribute('readonly');
   }
-}
+ }
 </script>
 <?= $this->endSection(); ?>
